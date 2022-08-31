623bfea85149bb545f1a450282b9b5b62e05502b
OmarMohamedoi <111138235+OmarMohamedoi@users.noreply.github.com>
2022-08-31 06:35:50 +0200

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -22,14 +22,13 @@
  * Define Global Variables
  * 
 */
-let sections1= document.querySelectorAll('section');
-let sectionslist1= document.querySelector('#navbar__list');
+let sections1= document.querySelectorAll('section'); // selecting all sections and storing them in a variable
+let sectionslist1= document.querySelector('#navbar__list'); // selecting the navbar and storign it in a variable for easier usage
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
 
 /**
  * End Helper Functions
@@ -40,19 +39,24 @@ let sectionslist1= document.querySelector('#navbar__list');
 // build the nav
 //built the nav using forEach()
 sections1.forEach(section => {
-    let element =document.createElement('li');
-    let anchor=document.createElement('a');
-    anchor.classList.add('menu__link')
-    anchorlink=section.getAttribute('id');
-    anchor.href="#" + anchorlink;
-    let name=section.getAttribute('data-nav');
-    anchor.textContent=name;
-    element.appendChild(anchor);
-    sectionslist1.appendChild(element);
+  let element =document.createElement('li'); //creating li element
+  let anchor=document.createElement('a'); //creating anchor element
+  anchor.classList.add('menu__link'); //using menu link for styling, also added cursor:pointer for more good looking
+  var anchorlink =section.getAttribute('id'); // storing the section's id in a variable
+  let name=section.getAttribute('data-nav'); // storing the section's name in a variable
+  anchor.textContent=name; // setting the text to the section's name
+  element.appendChild(anchor); // adding the anchor to the li element
+  sectionslist1.appendChild(element); // adding the li element to the navbar
+  element.addEventListener('click', (e)=>{ 
+    section.scrollIntoView({behavior:"smooth"})
+  }) 
+  
+  
 });
+ 
 // Add class 'active' to section when near top of viewport
 //checking if class is in view and if it's in view setting it as 'active'
-function checker(){
+function checker(){ // this function checks if the section active is in view and if it's in view adding class your-active-class to it
     sections1.forEach(section => {
         let area = section.getBoundingClientRect();
         if(area.top >=0&& area.top<300){
@@ -82,7 +86,7 @@ document.addEventListener('scroll', checker);
 // Set sections as active
 //checking if the section active is equal to the link for setting the link as active
 
-window.onscroll = function () {
+window.onscroll = function () { // this function checks if the active section is the same link for determining which link is the active link
     for (let section of sections1) {
       const sectionPos = section.getBoundingClientRect();
       let sectionTitle = section.getAttribute("data-nav");
@@ -98,3 +102,4 @@ window.onscroll = function () {
       }
     }
   };
+
